598b7234875b8be9e849b282fe1998bc9860236e
aswatanabe <awatanabegithub@gmail.com>
2023-08-28 10:32:15 +0900

    style: Modified main visual on the homepage

--- js/script.js
+++ js/script.js
@@ -2,8 +2,6 @@
 ハンバーガーメニュー
 ************************************************************************/
 
-/*--nav：テキストとハンバーガーメニューの切り替え------------------------ */
-
 window.addEventListener('load', changeBurger);
 window.addEventListener('resize', changeBurger);
 
@@ -12,19 +10,17 @@ function changeBurger() {
     let deviceW = 600;
 
     if (winW <= deviceW) {
-        //600px以下なら
+        // 600px以下なら
         document.querySelector('nav > div').setAttribute('id', 'burger');
         document.querySelector('nav > div > div:nth-of-type(1)').setAttribute('id', 'burger-btn');
         document.querySelector('nav > div > div:nth-of-type(2)').setAttribute('id', 'burger-menu');
     } else {
-        //600pxより大きいなら
+        // 600pxより大きいなら
         document.querySelector('nav > div').removeAttribute('id');
         document.querySelector('nav > div > div:nth-of-type(1)').removeAttribute('id');
         document.querySelector('nav > div > div:nth-of-type(2)').removeAttribute('id');
     }
 }
-
-/*--ハンバーガーメニューを開いたときの動き------------------------ */
 
 const burger = document.getElementById('burger');
 const burgerBlur = document.querySelectorAll('.burger-blur');
@@ -56,12 +52,66 @@ window.addEventListener('scroll', () => {
     if (inner_width <= 1025) {
         stickyList.style.position = 'absolute';
         stickyList.style.inlineSize = '100%';
-        stickyList.style.top = '480px'; //#sticky-list-wrapperのtopに合わせる
+        stickyList.style.top = '480px';
     } else if (inner_width >= 1025 && scroll_y >= 310) {
         stickyList.style.position = 'absolute';
         stickyList.style.top = '790px';
     } else if (inner_width >= 1025 && scroll_y < 310) {
         stickyList.style.position = 'fixed';
-        stickyList.style.top = '480px'; //#sticky-list-wrapperのtopに合わせる
+        stickyList.style.top = '480px';
     }
 });
+
+// 8.5秒後に表示を切り替える
+setTimeout(() => {
+    stickyList.style.display = 'block';
+    stickyList.style.opacity = '1';
+}, 8500);
+
+
+/************************************************************************
+タイプライターエフェクト
+************************************************************************/
+
+const typewriterWithDelay = (param) => {
+    setTimeout(() => {
+        const el = document.querySelector(param.el);
+        const speed = param.speed;
+        const string = param.string.split("");
+        let index = 0;
+
+        const typeNextChar = () => {
+            if (index < string.length) {
+                el.textContent += string[index];
+                index++;
+                setTimeout(typeNextChar, speed);
+            }
+        };
+
+        typeNextChar();
+    }, 500); // 0.5秒の遅延
+};
+
+typewriterWithDelay({
+    el: "#typewriter",
+    string: "A harmonious blend where creative vision and digital innovation unite.",
+    speed: 100
+});
+
+/************************************************************************
+動画再生
+************************************************************************/
+
+const movieElement = document.querySelector('.movie');
+const movieVideoElement = document.querySelector('.movie-video');
+
+// 10秒後に.movie-videoを表示して再生する
+setTimeout(() => {
+    // PNG画像を非表示にする
+    movieElement.style.background = 'none';
+    
+    // .movie-videoを表示して再生
+    movieVideoElement.style.display = 'block';
+    movieVideoElement.play();
+}, 10000); // 10秒後 (10000ミリ秒)
+
